16817214ca74f25bde8cdc226edd7f0ec8db1887
PrabhuChandran <prabhuc94@gmail.com>
2019-06-10 20:16:14 +0530

    DocPicker Added

---
+++ fontier/src/main/java/com/wee3ventures/fontier/labels/DocLabel.kt
@@ -0,0 +1,10 @@
+package com.wee3ventures.fontier.labels
+
+import java.io.Serializable
+
+
+/**
+ * Created by niraj on 29-04-2019.
+ */
+
+data class DocLabel(var colorRes: Int, var colorLightRes: Int, var text: String, var extType: String): Serializable
---
+++ fontier/src/main/java/com/wee3ventures/fontier/ui/components/folder/DocFolder.kt
@@ -0,0 +1,10 @@
+package com.wee3ventures.fontier.ui.components.folder
+
+/**
+ * Created by WANGSUN on 02-Apr-19.
+ */
+data class DocFolder(var id: String = "",
+                     var name: String = "",
+                     var path: String = "",
+                     var contentCount: Int = 0) {
+}
